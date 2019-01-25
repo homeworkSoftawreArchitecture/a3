5bc10c6166c4a77f0ae11ddcad0fa8f3254c5063
161250080 <161250080@smail.nju.edu.cn>
2019-01-25 23:05:01 +0800

    add word reader

--- assignment03/src/main/java/Main.java
+++ assignment03/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import model.PDFReader;
 import model.Reader;
+import model.WordReader;
 
 import java.io.IOException;
 
@@ -7,9 +8,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        Reader reader = new PDFReader();
+        Reader pdfReader = new PDFReader();
+        Reader wordReader = new WordReader();
+        /*
         try {
-            System.out.println(reader.read("/Users/apple/a3/assignment03/assignment-3.pdf"));
+            System.out.println(pdfReader.read("/Users/apple/a3/assignment03/assignment-3.pdf"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+        try {
+            System.out.println(wordReader.read("/Users/apple/a3/assignment03/Õ‚¡™’Àµ•.docx"));
         } catch (IOException e) {
             e.printStackTrace();
         }
